73c1a8551b409b50b21c49893a33c20d1db08f0c
memoTux <romeo@mendezfuentes.net>
2023-03-15 08:31:14 -0600

    add loading state to Page Products

--- pages/products.vue
+++ pages/products.vue
@@ -7,16 +7,19 @@ definePageMeta({
 const { data, pending } = useFetch('/api/listProducts')
 
 const products = computed(() => data.value?.listProducts?.items.filter(item => item?.type === 'PRODUCT'))
-
+const isLoading = computed(() => Boolean(products.value) || !pending.value)
 </script>
 
 <template>
-  <VContainer>
-    <VRow v-if="products || !pending" align="stretch" justify="start">
+  <VContainer style="min-height: 80vh; height: 100%;">
+    <VRow v-if="isLoading" align="stretch" justify="start">
       <VCol v-for="product in products"
         :key="product!.id" cols="12" md="4" lg="3">
         <ProductCard v-bind="product" />
       </VCol>
+    </VRow>
+    <VRow v-else align="center" justify="center" style="min-height: inherit;">
+      <VProgressCircular color="secondary-700" indeterminate :size="80" width="8" />
     </VRow>
   </VContainer>
 </template>
