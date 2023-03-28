8a959b9b80e30815c5de9acedd2b0534259e92c1
memoTux <romeo@mendezfuentes.net>
2023-03-28 17:16:18 -0600

    add Monthly page logic & components

--- pages/monthly.vue
+++ pages/monthly.vue
@@ -1,9 +1,69 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ListOverallSalesQuery } from '~~/graphql/types';
+// @ts-ignore
+import { lineY, ruleY } from "@observablehq/plot";
+import { DatePicker } from "v-calendar";
+
+definePageMeta({
+  title: 'Monthly Sales',
+  description: 'Chart of monthly sales'
+})
+
+const { data, pending } = await useFetch<ListOverallSalesQuery>('/api/list/overall', {
+  key: 'api:list:overall',
+  query: {
+    query: 'listOverallSales'
+  }
+})
+
+const plot = computed(() => {
+  if (!data.value?.listOverallSales?.items[0]?.dailyData?.length) return { marks: [] }
+
+  const linesData: { date: Date, total: number, type: 'sales' | 'units' }[] = []
+
+  const { monthlyData, year } = data.value.listOverallSales.items[0]
+
+  if (!monthlyData?.length) return { marks: [] }
+
+  monthlyData.forEach((data) => {
+    if (!data || !data.month) return
+    const dateFormatted = new Date(`${data.month} 1, ${year}`)
+    linesData.push({ date: dateFormatted, total: data.totalSales, type: 'sales' })
+    linesData.push({ date: dateFormatted, total: data.totalUnits, type: 'units' })
+  })
+
+  return {
+    x: {
+      label: 'Month →'
+    },
+    color: {
+      legend: true,
+      scheme: 'oranges',
+      tickFormat: (l: string) => `${l.toLocaleUpperCase()} ${l === 'sales' ? '($)' : '(Q)'}`,
+      style: {
+        fontSize: '1rem'
+      }
+    },
+    marks: [
+      lineY(linesData, { x: "date", y: "total", stroke: "type", marker: "circle" }),
+      ruleY([0])
+    ]
+  }
+})
+const masks = ref({
+  modelValue: 'YYYY-MM-DD',
+});
+</script>
 
 <template>
-  <div>
-    Page: foo
-  </div>
+  <VContainer>
+    <VRow v-if="pending || !data" justify="center" align="center">
+      <VProgressCircular size="65" color="secondary" indeterminate />
+    </VRow>
+    <VRow v-else>
+      <VCol cols="12">
+        <PlotLine v-bind="plot" />
+      </VCol>
+    </VRow>
+  </VContainer>
 </template>
-
-<style scoped></style>
