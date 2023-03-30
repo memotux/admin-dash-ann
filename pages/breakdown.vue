343f09a3a906937bf2413e2553aa2f133a4b7826
memoTux <romeo@mendezfuentes.net>
2023-03-29 21:42:22 -0600

    add Breakdown page graph

--- pages/breakdown.vue
+++ pages/breakdown.vue
@@ -1,16 +1,37 @@
 <script lang="ts" setup>
-import { ListOverallSalesQuery } from '~~/graphql/types';
-
+import { useTheme } from "vuetify";
 definePageMeta({
   title: 'Breakdown',
   description: 'Breakdown of Sales by Category'
 })
 
-const { data, pending } = await useFetch<ListOverallSalesQuery>('/api/list/overall', {
+const theme = useTheme()
+
+const container = ref<HTMLDivElement | null>(null)
+
+const { data, pending } = await useFetch('/api/list/overall', {
   key: 'api:list:overall',
   query: {
     query: 'listOverallSales'
-  }
+  },
+  transform: (data) => data.listOverallSales?.items[0]?.salesByCategory
+})
+
+const cats = Object.entries(data.value!).map(([cat, val]) => ([cat, val] as [string, number]))
+
+const plot = DonutChart(cats, {
+  width: 1024,
+  height: 720,
+  colors: [
+    theme.current.value.colors['secondary-300'],
+    theme.current.value.colors['secondary-400'],
+    theme.current.value.colors['secondary-500'],
+    theme.current.value.colors['secondary-600']
+  ]
+})
+
+onMounted(() => {
+  container.value?.append(plot)
 })
 </script>
 
@@ -19,10 +40,8 @@ const { data, pending } = await useFetch<ListOverallSalesQuery>('/api/list/overa
     <VRow v-if="pending || !data" justify="center" align="center">
       <VProgressCircular size="65" color="secondary" indeterminate />
     </VRow>
-    <VRow v-else>
-      <VCol cols="12">
-        <!-- <PlotLine v-bind="plot" /> -->
-      </VCol>
+    <VRow v-else justify="center" align="center">
+      <div ref="container"></div>
     </VRow>
   </VContainer>
 </template>
