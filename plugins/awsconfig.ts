c7c7baa17bbc99b95035d98e1b33a251926a2110
memoTux <romeo@mendezfuentes.net>
2023-04-09 20:15:31 -0600

    update add AWS Amplify SSR /server/api/load

--- plugins/awsconfig.ts
+++ plugins/awsconfig.ts
@@ -2,5 +2,5 @@ import { Amplify } from 'aws-amplify';
 import awsExports from '~/aws-exports';
 
 export default defineNuxtPlugin(() => {
-  Amplify.configure(awsExports);
+  Amplify.configure({ ...awsExports, ssr: true });
 })
